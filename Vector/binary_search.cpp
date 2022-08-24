7fced64bc0ae877b1c983f906053592a9a7c780e
Fajar <fajarmh2@gmail.com>
2022-08-24 12:27:41 +0700

    add binary search stl

---
+++ Vector/binary_search.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	vector<int> array = {1, 2, 3, 4, 7, 12, 16};
+	bool hasil = binary_search(array.begin(), array.end(), 1212);
+	cout << hasil << endl;
+
+}
