0e9e976c38adcb008600be845203f2c808ae916f
Fajar <fajarmh2@gmail.com>
2023-02-23 23:13:18 +0700

    clearBitsInRange function

--- Bits/bits.cpp
+++ Bits/bits.cpp
@@ -34,16 +34,26 @@ void clearLastIBits(int &n, int i) {
 	n = n & mask;
 }
 
+void clearBitsInRange(int &n, int i, int j) {
+	int a = ~(0) << j + 1;
+	int b = (1 << i) - 1;
+	int mask = a | b;
+	n = (n & mask);
+}
+
 int main() {
-	int n = 15;
-	int i;
-	cin >> i;
+	int n = 31;
+	int i=1;
+	int j = 3;
+	// cin >> i;
 
 	// cout << getIthBit(n, i) << endl;
 	// setIthBit(n, i);
 	// clearIthBit(n, i);
 	// updateIthBit(n, i, 1);
-	clearLastIBits(n, i);
+	// clearLastIBits(n, i);
+
+	clearBitsInRange(n, i, j);
 	cout << n << endl;
 
 	return 0;
