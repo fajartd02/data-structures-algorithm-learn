cef8acb550a203b486f85e52739d3f5a9443bbec
Fajar <fajarmh2@gmail.com>
2023-02-23 20:58:27 +0700

    add setIthBit

--- Bits/bits.cpp
+++ Bits/bits.cpp
@@ -6,12 +6,19 @@ int getIthBit(int n, int i) {
 	return (n & mask) > 0 ? 1 : 0;
 }
 
+void setIthBit(int &n, int i) {
+	int mask = (1 << i);
+	n = (n | mask);
+}
+
 int main() {
 	int n = 5;
 	int i;
 	cin >> i;
 
-	cout << getIthBit(n, i) << endl;
+	// cout << getIthBit(n, i) << endl;
+	setIthBit(n, i);
+	cout << n << endl;
 
 	return 0;
 }
